afe08bad9969df23241bdc10588ed284b7814189
Jimmy Perez Pierola <pila900@gmail.com>
2021-02-23 17:24:59 -0400

    updating name of  App

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hi!!</h1>
+                <h1>Hi2!!</h1>
             </div>
         )
     }
